cb8b3b5e4efcd7181888545e683f6b51a70be9a4
Kai <hahaout@hotmail.com>
2025-05-11 06:51:59 +0200

    feat(core): add backend

---
+++ backend/api/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return JsonResponse({
+        "message": "Welcome to the API",
+        "endpoints": {
+            "admin": "/admin/",
+            "api": "/api/"
+        }
+    })
